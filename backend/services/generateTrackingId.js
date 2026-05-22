0519e0fdb092dec0752f7f73a2acad3f68cce418
niles <nileshmkjee123@gmail.com>
2026-05-22 09:39:49 +0530

    Created a method to generate TrackingId of parcel

---
+++ backend/services/generateTrackingId.js
@@ -0,0 +1,5 @@
+export const generateTrackingId =() => {
+    const timestamp = Date.now().toString().slice(-6)
+    const random = Math.floor(100 + Math.random()*900)
+    return `TR-CRR-${timestamp}${random}`
+}
